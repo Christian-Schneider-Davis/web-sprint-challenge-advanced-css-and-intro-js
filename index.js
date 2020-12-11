6d5092d9ed8f99a2105b8996e03fac80794974fd
Christian Schneider-Davis <75326583+Christian-Schneider-Davis@users.noreply.github.com>
2020-12-11 13:00:29 -0600

    Save 9

--- index.js
+++ index.js
@@ -269,8 +269,8 @@ function get20s(boomersAge){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(kicked){
+  .pop
 }
    
 
